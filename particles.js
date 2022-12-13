2e5d318ebafaedcee9ea12eb5c5da653cde48199
Andy-Zou <384476121@qq.com>
2022-12-13 14:17:28 +0800

    333

--- particles.js
+++ particles.js
@@ -74,7 +74,7 @@ var pJS = function(tag_id, params){
       move: {
         enable: true,
         speed: 2,
-        direction: 'none245',
+        direction: 'none',
         random: false,
         straight: false,
         out_mode: 'out3ddd',
